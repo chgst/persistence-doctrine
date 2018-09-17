4594fadc4b3bc35ae224eb9401554eb677ff2a3f
Janusz Slota <janusz.slota@nixilla.com>
2018-09-17 11:34:08 +0100

    adds getIterator implementation

--- spec/Changeset/Event/ObjectRepositorySpec.php
+++ spec/Changeset/Event/ObjectRepositorySpec.php
@@ -8,7 +8,14 @@ use Changeset\Event\EventInterface;
 use Changeset\Event\EventTrait;
 use Changeset\Event\ObjectRepository;
 use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ODM\MongoDB\DocumentRepository;
+use Doctrine\ODM\MongoDB\Query\Builder;
+use Doctrine\ODM\MongoDB\Query\Query;
+use Doctrine\ORM\AbstractQuery;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
 
 class ObjectRepositorySpec extends ObjectBehavior
 {
@@ -33,6 +40,68 @@ class ObjectRepositorySpec extends ObjectBehavior
         $manager->flush($event)->shouldBeCalled();
 
         $this->append($event);
+    }
+
+    function it_throws_exception_if_it_cannot_construct_iterator()
+    {
+        $this
+            ->shouldThrow(\InvalidArgumentException::class)
+            ->during('getIterator')
+        ;
+    }
+
+    function it_can_get_iterator_for_the_event_stream_if_it_is_defined(
+        ObjectManager $manager,
+        \IteratorAggregate $repo1,
+        \Iterator $iterator
+    )
+    {
+        $manager->getRepository(Argument::any())->willReturn($repo1);
+
+        $repo1->getIterator()->willReturn($iterator);
+
+        $this->getIterator();
+
+    }
+
+    function it_can_get_iterator_for_the_event_stream_ORM(
+        ObjectManager $manager,
+        EntityRepository $repository,
+        QueryBuilder $queryBuilder,
+        AbstractQuery $query,
+        \Iterator $iterator
+    )
+    {
+        $manager->getRepository(Argument::any())->willReturn($repository);
+
+        $repository->createQueryBuilder(Argument::any())->willReturn($queryBuilder);
+
+        $queryBuilder->getQuery()->willReturn($query);
+        $queryBuilder->orderBy(Argument::any())->shouldBeCalled()->willReturn($queryBuilder);
+
+        $query->iterate()->willReturn($iterator);
+
+        $this->getIterator();
+    }
+
+    function it_can_get_iterator_for_the_event_stream_MongoDB(
+        ObjectManager $manager,
+        DocumentRepository $repository,
+        Builder $queryBuilder,
+        Query $query,
+        \Iterator $iterator
+    )
+    {
+        $manager->getRepository(Argument::any())->willReturn($repository);
+
+        $repository->createQueryBuilder(Argument::any())->willReturn($queryBuilder);
+
+        $queryBuilder->getQuery()->willReturn($query);
+        $queryBuilder->sort(Argument::any(), Argument::any())->shouldBeCalled()->willReturn($queryBuilder);
+
+        $query->getIterator()->willReturn($iterator);
+
+        $this->getIterator();
     }
 }
 
--- src/Changeset/Event/ObjectRepository.php
+++ src/Changeset/Event/ObjectRepository.php
@@ -3,6 +3,8 @@
 namespace Changeset\Event;
 
 use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ODM\MongoDB\Query\Builder;
+use Doctrine\ORM\QueryBuilder;
 
 class ObjectRepository implements RepositoryInterface
 {
@@ -33,5 +35,51 @@ class ObjectRepository implements RepositoryInterface
     {
         $this->manager->persist($event);
         $this->manager->flush($event);
+    }
+
+    public function getIterator(): \Iterator
+    {
+        $repository = $this->manager->getRepository($this->eventClass);
+
+        if (method_exists($repository, 'getIterator'))
+        {
+            return $repository->getIterator();
+        }
+
+        if (is_subclass_of($repository, 'Doctrine\ODM\MongoDB\DocumentRepository'))
+        {
+            return $this->getMongoDbIterator($repository);
+        }
+
+        if(is_subclass_of($repository, 'Doctrine\ORM\EntityRepository'))
+        {
+            return $this->getORMIterator($repository);
+        }
+
+        throw new \InvalidArgumentException('Unable to construct iterator');
+    }
+
+    private function getMongoDbIterator($repository)
+    {
+        /** @var Builder $qb */
+        $qb = $repository->createQueryBuilder('e');
+
+        return $qb
+            ->sort('createdAt', 'asc')
+            ->getQuery()
+            ->getIterator()
+        ;
+    }
+
+    private function getORMIterator($repository)
+    {
+        /** @var QueryBuilder $qb */
+        $qb = $repository->createQueryBuilder('e');
+
+        return $qb
+            ->orderBy('e.createdAt ASC')
+            ->getQuery()
+            ->iterate()
+        ;
     }
 }
